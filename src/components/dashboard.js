37d20778b3e1a86402b2a2b0cd03478fbaec21af
ramanadhanateam <ramanadhan@ateamsoftsolutions.com>
2022-03-11 16:23:05 +0530

    users list

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,22 +1,56 @@
-import React, {useEffect} from "react";
+import React, {useEffect,useState} from "react";
 import  Axios  from "axios";
 
 
-function Dash(){
-    
-    useEffect=(()=>{
+function Dash() {
+    const [data,setDate]=useState([])
+    useEffect(()=>{
         Axios.get('https://jsonplaceholder.typicode.com/users')
-        .then(res=>
-          
-            console.log(res.data)).catch(err=>console.log(err))
+        .then(res=>{
+        console.log("getting from::",res.data)
+        setDate(res.data)
+        })
+        .catch(err=>console.log(err))
+
+    },[])
     
-      },[])
+    const array= data.length>0 && data.map((data,index)=>{
+        return(
+            
+            <tr>
+                
+                <td>{data.id}</td>
+                <td>{data.name}</td>
+                <td>{data.address.street}</td>
+               
+   
+   
+   
+  </tr>
+  
+
+        )
+    })    
 
         
    
     return(
-<div >
+<div  >
         <h1>DASHBOARD</h1>
+        
+<table>
+  <tr>
+   
+    <th>id</th>
+    <th>name</th>
+    <th>address</th>
+  
+    
+  </tr>
+  
+  {array}
+  
+</table>
         
       
                  
