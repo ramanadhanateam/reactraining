c3a899576ebec187c46414fdbf0e3573b2d841bd
ramanadhanateam <ramanadhan@ateamsoftsolutions.com>
2022-03-14 10:21:56 +0530

    users list using mui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Login from'./components/login';
-import Dash from'./components/dashboard';
+import Login from'./components/Login';
+import DashBoard from'./components/DashBoard';
 import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   <Router>
     <Routes>
      <Route path="/login" element={<Login/>} />
-     <Route path="/dash" element={<Dash/>} />
+     <Route path="/dash" element={<DashBoard/>} />
      </Routes>
      </Router>
   
