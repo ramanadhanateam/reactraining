6088d47e53f2b35d27b024ff1bb419cf0f7dca3c
ramanadhanateam <ramanadhan@ateamsoftsolutions.com>
2022-03-15 16:51:42 +0530

    pagination

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './App.css';
-import Login from'./components/Login';
-import DashBoard from'./components/DashBoard';
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import Login from './components/Login';
+import DashBoard from './components/DashBoard';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-  <Router>
-    <Routes>
-     <Route path="/login" element={<Login/>} />
-     <Route path="/dash" element={<DashBoard/>} />
-     </Routes>
-     </Router>
-  
- 
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={< Login />}/>
+                <Route path="/dash" element={< DashBoard />}/>
+            </Routes>
+        </Router>
+
+    );
 }
 
 export default App;
