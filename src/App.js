35addf939c7ab58f20165838d0ae15f2ad0a73fb
ramanadhanateam <100823022+ramanadhanateam@users.noreply.github.com>
2022-03-23 16:56:12 +0530

    Update App.js
    changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/login" element={< Login />}/>
+                <Route path="/login" element={< signin />}/>
                 <Route path="/dash" element={< DashBoard />}/>
             </Routes>
         </Router>
